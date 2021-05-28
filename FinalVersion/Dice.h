d409e245fe8179c46c81092cb4a98ed072223d8b
justNeto <49221536+justNeto@users.noreply.github.com>
2021-05-28 13:37:21 -0500

    Add files via upload
    Final Version

---
+++ FinalVersion/Dice.h
@@ -0,0 +1,18 @@
+//
+// Created by conti on 5/24/2021.
+//
+
+#ifndef SNAKES_LADDERS_DICE_H
+#define SNAKES_LADDERS_DICE_H
+
+
+class Dice {
+public:
+    int diceVal;
+    Dice();
+    void rollDice();
+    int getDiceVal();
+};
+
+
+#endif //SNAKES_LADDERS_DICE_H
